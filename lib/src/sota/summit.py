91e893bb42e75813c48773ce4f221b27b99e7750
Piotr Majkrzak <piotr@majkrzak.dev>
2024-11-25 16:26:00 +0100

    Reorganize summit fetching code

--- lib/src/sota/summit.py
+++ lib/src/sota/summit.py
@@ -1,21 +1,78 @@
 from dataclasses import dataclass, field
 from typing import Self, Iterable, Optional
-from pandas import DataFrame
+from pandas import DataFrame, read_csv
 from shapely import Point
 
-from .helpers.fetch_summits import fetch_summits
 from .zone import Zone
 from .gmina import Gmina
 from .park import Park
-from .helpers.cache import pickled
+from .helpers.cache import pickled, download
 from .reference import Reference
 
 
 __all__ = ["Summit"]
 
 
+def _fetch_summits() -> DataFrame:
+    summits = read_csv(
+        download("https://storage.sota.org.uk/summitslist.csv"),
+        low_memory=False,
+        skiprows=[0],
+        index_col=0,
+    )
+
+    # Select covered summits
+    summits = summits[
+        (summits.AssociationName == "Poland")
+        | (
+            summits.index.isin(
+                [  # border summits (maybe auto based on distance?)
+                    "OK/LI-003",
+                    "OK/KR-001",
+                    # "OK/OL-011", # Partially in Poland, no height data for peak available
+                    "OK/OL-022",
+                    "OK/MO-061",
+                    "OM/ZA-045",
+                    "OM/ZA-059",
+                    "OM/ZA-061",
+                    "OM/ZA-023",
+                    "OM/ZA-046",
+                    "OM/ZA-081",
+                    "OM/ZA-005",
+                    "OM/PO-012",
+                    "OM/PO-054",
+                    "OM/PO-013",
+                    "OM/PO-092",
+                    "OM/PO-040",
+                    # "UT/CA-189", # Partially in Poland, no height data for peak available
+                    # "UT/CA-234", # Border peek, part of zone height data missing
+                ]
+            )
+        )
+    ]
+
+    # Remove invalid summits
+    summits = summits[summits.ValidTo == "31/12/2099"]
+
+    # Clean unnecessary columns
+    summits = summits.drop(
+        columns=[
+            "AltFt",
+            "GridRef1",
+            "GridRef2",
+            "ValidFrom",
+            "ValidTo",
+            "ActivationCount",
+            "ActivationDate",
+            "ActivationCall",
+        ]
+    )
+
+    return summits
+
+
 class MetaSummit(type):
-    SUMMITS: DataFrame = pickled("SUMMITS", fetch_summits)
+    SUMMITS: DataFrame = pickled("SUMMITS", _fetch_summits)
 
     def __getitem__(cls, reference: Reference) -> Self:
         if f"{reference}" not in cls.SUMMITS.index:
