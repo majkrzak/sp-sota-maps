200cfb5aa395a409f669de13e592c216ac950082
Piotr Majkrzak <piotr@majkrzak.dev>
2024-11-27 21:06:26 +0100

    Use realative osm carto load

--- lib/src/sota/render_carto.cpp
+++ lib/src/sota/render_carto.cpp
@@ -20,8 +20,8 @@ PyObject *render_carto(PyObject *self, PyObject *args) {
   mapnik::Map map(width, height);
 
   mapnik::load_map(map,
-                   "/home/majkrzak/Development/openstreetmap-carto/carto.xml",
-                   false, "/home/majkrzak/Development/openstreetmap-carto");
+                   "../openstreetmap-carto/carto.xml",
+                   false, "../openstreetmap-carto");
   map.set_srs(epsg);
   map.zoom_to_box(mapnik::box2d<double>(xl, yl, xh, yh));
   mapnik::save_to_cairo_file(map, file);
