147e639822712ba63032443a20ab48a3039d3849
Piotr Majkrzak <piotr@majkrzak.dev>
2024-12-30 12:33:04 +0100

    Add page structure

---
+++ web/src/components/downloads.rs
@@ -0,0 +1,9 @@
+use yew::{function_component, html, Html};
+
+#[function_component(Downloads)]
+pub fn component() -> Html {
+    html! {
+        <>
+        </>
+    }
+}
--- web/src/components/mod.rs
+++ web/src/components/mod.rs
@@ -1,1 +1,4 @@
+pub mod about;
 pub mod app;
+pub mod downloads;
+pub mod not_found;
