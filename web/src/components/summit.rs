291df30a46cb7cb94a51e4da576f78af9e5ace8f
Piotr Majkrzak <piotr@majkrzak.dev>
2024-12-31 11:50:08 +0100

    Add simple summit view

--- web/src/components/summit.rs
+++ web/src/components/summit.rs
@@ -10,8 +10,20 @@ pub struct Props {
 #[function_component(Summit)]
 pub fn component(props: &Props) -> Html {
     html! {
-        <>
-            {props.reference.full()}
-        </>
+        <article>
+            <h3>{props.reference.full()}</h3>
+            <nav>
+                <a style="margin:0 .5em" href={format!("https://www.sotadata.org.uk/summit/{}",props.reference.full())}>{"sotadata.org.uk"}</a>
+                <a style="margin:0 .5em" href={format!("https://sotl.as/summits/{}",props.reference.full())}>{"sotl.as"}</a>
+            </nav>
+            <img src={format!("https://github.com/majkrzak/sp-sota-maps/releases/download/0.0.0/{}.png",props.reference.slug())}/>
+            <section>
+                <h4>{"Downloads"}</h4>
+                <nav>
+                    <a class="button" style="margin:0 .5em" href={format!("https://github.com/majkrzak/sp-sota-maps/releases/download/0.0.0/{}.pdf",props.reference.slug())}>{"A5 PDF"}</a>
+                    <a class="button" style="margin:0 .5em" href={format!("https://github.com/majkrzak/sp-sota-maps/releases/download/0.0.0/{}.png",props.reference.slug())}>{"1440p PNG"}</a>
+                </nav>
+            </section>
+        </article>
     }
 }
