f61e33a2db402798a76cb7d8b361ff829a9115b5
Piotr Majkrzak <piotr@majkrzak.dev>
2025-03-16 13:59:04 +0100

    Add Remove precalculated summit isnisghts

--- web/src/model/summit.rs
+++ web/src/model/summit.rs
@@ -9,20 +9,4 @@ pub struct Summit {
     pub lat: f32,
     pub lon: f32,
     pub alt: f32,
-    pub insights: SummitInsights,
-    pub hmap: SummitHmapInfo,
-}
-
-#[derive(Debug, Clone, PartialEq, Deserialize)]
-pub struct SummitInsights {
-    pub elevation: f32,
-    pub distance: f32,
-    pub total_area: f32,
-    pub polish_area: f32,
-}
-
-#[derive(Debug, Clone, PartialEq, Deserialize)]
-pub struct SummitHmapInfo {
-    pub symbols: Vec<String>,
-    pub reports: Vec<String>,
 }
