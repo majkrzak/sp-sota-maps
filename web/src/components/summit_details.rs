664b0a431c49248ca7f8aada41b76a8820d737af
Piotr Majkrzak <piotr@majkrzak.dev>
2025-06-19 11:07:50 +0200

    Separate summit view components

--- web/src/components/summit_details.rs
+++ web/src/components/summit_details.rs
@@ -3,8 +3,8 @@ use yew::{function_component, html, use_context, Html, Properties};
 
 use crate::{
     helpers::catch::catch,
-    model::{reference::Reference, release::Release, summit::Summit, summits::Summits},
-    views::summit::summit_brief,
+    model::{reference::Reference, release::Release, summits::Summits},
+    views::summit::{summit_brief, summit_downloads, summit_image, summit_links},
 };
 
 #[derive(Properties, PartialEq, Clone)]
@@ -23,48 +23,11 @@ pub fn component(props: &Props) -> Html {
         Ok(html! {
             <article>
                 <h3>{ summit_brief(&summit) }</h3>
-                <nav>
-                    <a
-                        style="margin:0 .5em"
-                        href={format!("https://www.sotadata.org.uk/summit/{}",summit.reference.full())}
-                    >
-                        { "sotadata.org.uk" }
-                    </a>
-                    <a
-                        style="margin:0 .5em"
-                        href={format!("https://sotl.as/summits/{}",summit.reference.full())}
-                    >
-                        { "sotl.as" }
-                    </a>
-                </nav>
-                <img
-                    src={release.asset(format!("{}.avif",summit.reference.slug()).as_str()).unwrap().browser_download_url.clone()}
-                />
+                <nav>{ summit_links(&summit) }</nav>
+                { summit_image(&release, &summit) }
                 <section>
                     <h4>{ "Downloads" }</h4>
-                    <nav>
-                        <a
-                            class="button"
-                            style="margin:0 .5em"
-                            href={release.asset(format!("{}.pdf",summit.reference.slug()).as_str()).unwrap().browser_download_url.clone()}
-                        >
-                            { "A5 PDF" }
-                        </a>
-                        <a
-                            class="button"
-                            style="margin:0 .5em"
-                            href={release.asset(format!("{}.png",summit.reference.slug()).as_str()).unwrap().browser_download_url.clone()}
-                        >
-                            { "1440p PNG" }
-                        </a>
-                        <a
-                            class="button"
-                            style="margin:0 .5em"
-                            href={release.asset(format!("{}.avif",summit.reference.slug()).as_str()).unwrap().browser_download_url.clone()}
-                        >
-                            { "1440p AVIF" }
-                        </a>
-                    </nav>
+                    <nav>{ summit_downloads(&release, &summit) }</nav>
                 </section>
             </article>
         })
--- web/src/views/summit.rs
+++ web/src/views/summit.rs
@@ -1,7 +1,10 @@
 use yew::{html, Html};
 use yew_router::prelude::Link;
 
-use crate::{model::summit::Summit, router::Route};
+use crate::{
+    model::{release::Release, summit::Summit},
+    router::Route,
+};
 
 pub fn summit_brief(summit: &Summit) -> Html {
     html! {
@@ -13,6 +16,61 @@ pub fn summit_brief(summit: &Summit) -> Html {
             { summit.name.clone() }
             { " – " }
             { format!("{:0.0}m",summit.alt) }
+        </>
+    }
+}
+
+pub fn summit_links(summit: &Summit) -> Html {
+    html! {
+        <>
+            <a
+                style="margin:0 .5em"
+                href={format!("https://www.sotadata.org.uk/summit/{}",summit.reference.full())}
+            >
+                { "sotadata.org.uk" }
+            </a>
+            <a
+                style="margin:0 .5em"
+                href={format!("https://sotl.as/summits/{}",summit.reference.full())}
+            >
+                { "sotl.as" }
+            </a>
+        </>
+    }
+}
+
+pub fn summit_image(release: &Release, summit: &Summit) -> Html {
+    html! {
+        <img
+            src={release.asset(format!("{}.avif",summit.reference.slug()).as_str()).unwrap().browser_download_url.clone()}
+        />
+    }
+}
+
+pub fn summit_downloads(release: &Release, summit: &Summit) -> Html {
+    html! {
+        <>
+            <a
+                class="button"
+                style="margin:0 .5em"
+                href={release.asset(format!("{}.pdf",summit.reference.slug()).as_str()).unwrap().browser_download_url.clone()}
+            >
+                { "A5 PDF" }
+            </a>
+            <a
+                class="button"
+                style="margin:0 .5em"
+                href={release.asset(format!("{}.png",summit.reference.slug()).as_str()).unwrap().browser_download_url.clone()}
+            >
+                { "1440p PNG" }
+            </a>
+            <a
+                class="button"
+                style="margin:0 .5em"
+                href={release.asset(format!("{}.avif",summit.reference.slug()).as_str()).unwrap().browser_download_url.clone()}
+            >
+                { "1440p AVIF" }
+            </a>
         </>
     }
 }
